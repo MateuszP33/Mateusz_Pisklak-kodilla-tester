961eae25fca8a7da35c32ae80340c8b1b0100e4d
Mateusz Pisklak <mateuszpisklak@wp.pl>
2023-12-17 19:58:59 +0100

    second task

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String book = "Wartka śmierć";
+        int age = 33;
+        char letter = 'M';
+        double score = 5.5d;
+        boolean weAreTheBest = true;
+        String month = "december";
+        System.out.println(book);
+        System.out.println(age);
+        System.out.println(letter);
+        System.out.println(score);
+        System.out.println(weAreTheBest);
+        System.out.println(month);
+    }
+}
