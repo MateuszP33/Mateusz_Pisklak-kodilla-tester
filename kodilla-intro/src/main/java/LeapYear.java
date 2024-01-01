5002aebf97fd49e6253199932f068890a90b5ddd
Mateusz Pisklak <mateuszpisklak@wp.pl>
2024-01-01 16:51:27 +0100

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,20 @@
+public class LeapYear {
+    public static void main(String[] args) {
+
+            int a = 758;
+
+            if (a % 4 == 0) {
+                if (a % 100 == 0) {
+                    if (a % 400 == 0) {
+                        System.out.println("Rok " + a + " jest przestępny");
+                    } else {
+                        System.out.println("Rok " + a + " nie jest przestępny");
+                    }
+                } else {
+                    System.out.println("Rok " + a + " jest przestępny");
+                }
+            } else {
+                System.out.println("Rok " + a + " nie jest przestępny");
+            }
+    }
+}
