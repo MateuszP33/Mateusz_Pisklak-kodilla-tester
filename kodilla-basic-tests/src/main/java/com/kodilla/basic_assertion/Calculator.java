f0940b6db2bb5632d7ccc48cc459a02fe2427688
Mateusz Pisklak <mateuszpisklak@wp.pl>
2024-01-21 13:07:57 +0100

    task 3.5

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int power(int a) {
+        return (int) Math.pow(a,2);
+    }
+}
